4d938a4ad51bb95701e6a6d2cf9c146607ba0978
vkarunachalam <60417245+vkarunachalam@users.noreply.github.com>
2020-01-29 10:06:44 +0530

    Create hangman game.py

---
+++ hangman game.py
@@ -0,0 +1,18 @@
+import random
+
+strs = ['dad', 'mom', 'bat', 'car', 'bus', 'pen', 'dog', 'cat']
+points = 0
+string = random.choice(strs)
+
+for i in range(3):
+    c = input('Enter a character: ')
+    if c in string:
+        print('Good')
+        points = points + 1
+    else:
+        print('You entered a wrong character')
+        
+if points == 3:
+    print('Won')
+else:
+    print('Lost')
